ac914d814834bb8ec5af8874ac668772d66a6270
Emilio <erz_18@outlook.com>
2024-06-02 16:53:07 +0200

    Add files via upload

---
+++ target.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+#coding: utf-8
+
+import sys
+import ipaddress
+import subprocess
+
+if len(sys.argv) != 2:
+    print("\n[!] Uso: python3 "+ sys.argv[0] + "<ip objetivo>\n")
+    sys.exit(1)
+
+ip_objetivo = sys.argv[1]
+
+try:
+    ip = ipaddress.ip_address(ip_objetivo)
+except ValueError:
+    print("\n[!] IP objetivo inválida")
+    sys.exit(1)
+
+try:
+    subprocess.run(['qtile', 'cmd-obj', '-o', 'widget', 'textbox', '-f', 'update', '-a', ip_objetivo], check=True)    
+    print(f"El texto del widget se actualizó a: {ip_objetivo}")
+except subprocess.CalledProcessError as e:
+    print(f"\n[!] Error al ejecutar el comando de Qtile: {e}")
+    sys.exit(1)
+
